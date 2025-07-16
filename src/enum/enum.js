c6412328c418a321da435cdfbfe0f1fbe093f7db
Md Julfikar Rahman <rjulfikar30@gmail.com>
2025-07-16 20:37:36 +0600

    zustand deye shopping card

--- src/enum/enum.js
+++ src/enum/enum.js
@@ -1,26 +1,35 @@
-import headphone from "../assets/1.jpg";
+import headphone from "../assets/1.png"
 import Watch from "../assets/2.jpeg";
 import Speaker from "../assets/3.jpeg";
+import iphone from "../assets/4.jpeg";
 export const products =[
     {
         id: 1,
         title: "Wireless Headphones",
         price: 29.99,
         description: "This is a great product.",
-        image: "headphone "
+        image: headphone 
     },
     {
         id: 2,
         title: "Smart Watch",
         price: 49.99,
         description: "This is another great product.",
-        image: "Watch"
+        image: Watch
     },
     {
         id: 3,
         title: "Bluetooth Speaker",
         price: 19.99,
         description: "This is yet another great product.",
-        image: "Speaker"
-    }
+        image: Speaker
+    },
+    {
+        id: 4,
+        title: "iPhone 15 Pro Max",
+        price: 1099.99,
+        description: "This is yet another great product.",
+        image: iphone
+    },
+
 ]
